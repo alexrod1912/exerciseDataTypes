96c65f992a8e411853ac90e25ae54c751e127d09
Naomi Rodriguez <naomirodriguez@Naomis-MacBook-Air.local>
2024-05-21 16:20:10 -0500

    able to ask user in console for name as well as length and width of rectangle and output the rectangles area

---
+++ helloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, Naomi!");
+Console.WriteLine("What is your name");
+string name = Console.ReadLine();
+Console.WriteLine("Hello " + name);
+Console.WriteLine("Please provide a length value for your rectangle");
+string length = Console.ReadLine();
+int lengthInt = int.Parse(length);
+Console.WriteLine("Please provide a width ");
+string width = Console.ReadLine();
+int widthInt = int.Parse(width);
+Console.WriteLine("Your rectangles area is " + widthInt * lengthInt);
