fd69670abb67a52fc43f4ac5497bf0c3437fcfa0
Naomi Rodriguez <naomirodriguez@Naomis-MacBook-Air.local>
2024-05-28 19:29:18 -0500

    studio for control flow chapter three is complete without the bonus mission. We are able to search the given string and count how many times the different characters show up

---
+++ StudioCh3/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string testString = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc accumsan sem ut ligula scelerisque sollicitudin. Ut at sagittis augue. Praesent quis rhoncus justo. Aliquam erat volutpat. Donec sit amet suscipit metus, non lobortis massa. Vestibulum augue ex, dapibus ac suscipit vel, volutpat eget massa. Donec nec velit non ligula efficitur luctus.";
+Dictionary<string, int> storeString = new Dictionary<string, int>();
+
+
+foreach(char letter in testString){
+    // int count =0; 
+    string charac = letter.ToString();
+    if(!storeString.ContainsKey(charac)){
+        storeString.Add(charac, 1);
+    }else if (storeString.ContainsKey(charac)){
+        storeString[charac] =  storeString[charac] + 1;
+        
+    };
+}
+
+foreach(KeyValuePair<string, int> characterInt in storeString){
+    Console.WriteLine(characterInt.Key+ " : " + characterInt.Value);
+}
+
--- helloWorld/Program.cs
+++ helloWorld/Program.cs
@@ -13,21 +13,25 @@
 string originalString = "Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do: once or twice she had peeped into the book her sister was reading, but it had no pictures or conversations in it, 'and what is the use of a book,' thought Alice 'without pictures or conversation?'";
 Console.WriteLine("Please type a word that you want to search our string for");
 string searchWord = Console.ReadLine();
+
 if(originalString.ToLower().Contains(searchWord.ToLower())){
     int indexSearchWord = originalString.ToLower().IndexOf(searchWord.ToLower());
     int lengthOfSearchWord = searchWord.Length;
     string lowerString = originalString.ToLower();
     string[] trimmedString = lowerString.Split(searchWord);
     string? concatString = null;
+
     foreach(string line in trimmedString){
         // Console.WriteLine(line);
         concatString = concatString + line;
         string concatTrimmedString;
     }
-    Console.WriteLine(concatString + "Here");
+
+    // Console.WriteLine(concatString + "Here");
     // Console.WriteLine(trimmedString.Count());
     // Console.WriteLine(trimmedString);
-    Console.WriteLine(searchWord + ": is in our original string and is at index: " + indexSearchWord + "and has a length of: " + lengthOfSearchWord);
+    
+    Console.WriteLine(searchWord + ": is in our original string and is at index: " + indexSearchWord + " and has a length of: " + lengthOfSearchWord);
     Console.WriteLine("New String: " + concatString);
 }else{
     Console.WriteLine("That search word was not found");
